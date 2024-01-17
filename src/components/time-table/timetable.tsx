3ca3da7356dc54857fc832b196e49eebbd67c9e8
yesl-kim <yesl00k624@gmail.com>
2024-01-17 13:42:28 +0900

    minor: timetable constants 정리

--- src/components/time-table/timetable.tsx
+++ src/components/time-table/timetable.tsx
@@ -1,14 +1,11 @@
 'use client'
 
-import { addMinutes, eachMinuteOfInterval, format } from 'date-fns'
+import { addMinutes } from 'date-fns'
 import { MouseEventHandler, useState } from 'react'
 
 import useToday from '@/hooks/useToday'
-import useBoolean from '@/hooks/useBoolean'
 
-const HEIGHT = 48
-const $30_MINUTES_HEIGHT = HEIGHT / 2
-const HEIGHT_PER_BLOCK = HEIGHT / 2 + 'px'
+import { HEIGHT_PER_STEP, STEP, TOTAL_HEIGHT } from './timetable.constants'
 
 type TimeTableProps = {
   date: Date
@@ -16,35 +13,25 @@ type TimeTableProps = {
 }
 
 //? 선은 어디서 렌더해야되지
-// offset y -> duration (분) -> 30분 내림 계산
-// 1분 = 48 / 60
-
-// TODO: height 단위 정리
 const Timetable = ({ date, newBlock }: TimeTableProps) => {
   const { getStartOfDay } = useToday()
-
-  const totalHeight = HEIGHT * 24 + 'px'
   const [offsetY, setOffsetY] = useState<null | number>(null)
 
   const onClick: MouseEventHandler<HTMLDivElement> = (e) => {
     setOffsetY(e.nativeEvent.offsetY)
   }
 
-  const getY = (offsetY: number, step = 30) => {
-    const heightPerStep = HEIGHT / (60 / step)
-    return Math.floor(offsetY / heightPerStep) * heightPerStep
-  }
+  const getY = (offsetY: number) =>
+    Math.floor(offsetY / HEIGHT_PER_STEP) * HEIGHT_PER_STEP
 
-  // reducer를 쓰는게 더 깔끔하려나
-  const getSelectedTime = (offsetY: number, step = 30) => {
-    const heightPerStep = HEIGHT / (60 / step)
-    const duration = Math.floor(offsetY / heightPerStep) * step
+  const getSelectedTime = (offsetY: number) => {
+    const duration = Math.floor(offsetY / HEIGHT_PER_STEP) * STEP
     const time = addMinutes(getStartOfDay(date), duration)
     return time
   }
 
   return (
-    <div className="relative bg-white" style={{ height: totalHeight }}>
+    <div className="relative bg-white" style={{ height: `${TOTAL_HEIGHT}px` }}>
       <div className="absolute inset-0" onClick={onClick}>
         {offsetY && (
           <div
