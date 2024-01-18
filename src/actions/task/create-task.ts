6e935fe43980b0e3fbd23f50a3f457bf98744f56
yesl-kim <yesl00k624@gmail.com>
2024-01-18 23:57:59 +0900

    fix: task 생성 시 startDate 값 수정
    - 시작일의 기본값은 <오늘>
    - 서버에서 저장시 시간 제외 (0시로 저장)

--- src/actions/task/create-task.ts
+++ src/actions/task/create-task.ts
@@ -4,6 +4,7 @@ import { withAuth } from '@/lib/auth'
 import prisma from '@/lib/prisma'
 import { ValidationError, fromZodError } from 'zod-validation-error'
 import { z } from 'zod'
+import { startOfDay } from 'date-fns'
 
 import {
   TaskCreateInput,
@@ -19,19 +20,25 @@ export const createTask = async (
   data: TaskCreateInput
 ): Promise<CreateTaskResponse> => {
   try {
-    const schemaWithAuth = withAuth(taskCreateInputSchema)
+    const schemaWithAuth = withAuth(taskCreateInputSchema).refine((task) => {
+      const repeats = task.repeats.map((r) => ({
+        ...r,
+        startDate: startOfDay(r.startDate),
+      }))
+      return { ...task, repeats }
+    })
 
     const {
       title,
       color,
-      repeats,
       category,
+      repeats,
       session: {
         user: { email },
       },
     } = await schemaWithAuth.parseAsync(data)
 
-    const task = await prisma.task.create({
+    await prisma.task.create({
       data: {
         title,
         color,
