0187ca6e650d47677e51366eaad8f0f860372ed3
yesl-kim <yesl00k624@gmail.com>
2024-02-22 15:31:32 +0900

    style: 일간 스케줄 레이아웃

--- src/app/schedule/day/[[...dateArgs]]/page.tsx
+++ src/app/schedule/day/[[...dateArgs]]/page.tsx
@@ -1,8 +1,8 @@
-import { set, startOfDay } from 'date-fns'
+import { format } from 'date-fns'
 
+import CalendarNavigator from '@/containers/navigation-calendar/navigator'
 import DailyTaskRecordTimetable from '@/containers/daily-task-record-timetable'
 import NavigationCalendar from '@/containers/navigation-calendar/navigation-calendar'
-import CalendarNavigator from '@/containers/navigation-calendar/navigator'
 import NewButton from '@/containers/new-task/new-button'
 import { parmasToDate } from '@/hooks/use-navigate-date/use-navigate-date.utils'
 
@@ -13,17 +13,19 @@ type Params = {
 export default async function DailySchedulePage({ params }: Params) {
   const date = parmasToDate(params) ?? new Date()
   return (
-    <>
-      <nav>
+    <div className="flex flex-1 flex-col bg-white">
+      <header className="mb-4 flex min-h-[60px] items-center gap-4 border-b-[1px] border-gray-200 px-8">
         <CalendarNavigator type="daily" />
-      </nav>
-      <main className="flex flex-1 gap-1 overflow-hidden">
-        <aside>
+        <h1>{format(date, 'yyyy년 MM월 dd일')}</h1>
+      </header>
+
+      <div className="flex flex-1 overflow-hidden">
+        <aside className="flex flex-col gap-4 px-2">
           <NewButton />
           <NavigationCalendar />
         </aside>
         <DailyTaskRecordTimetable date={date} />
-      </main>
-    </>
+      </div>
+    </div>
   )
 }
